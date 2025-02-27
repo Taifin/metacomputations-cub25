d5ece573528292dea58a1d7babeb58312d71c0d5
Mikhail Rodionychev <tralllgm@gmail.com>
2025-02-27 19:09:37 +0100

    add test for first projection

--- src/test/kotlin/InterpretationTest.kt
+++ src/test/kotlin/InterpretationTest.kt
@@ -12,6 +12,7 @@ class InterpretationTest {
     private val stdin = System.`in`
     private val tmDir = Paths.get("src/test/resources/tm")
     private val tmInterpreter = "turing-machine-int.fchart"
+    private val mix = Paths.get("src/test/resources/mix.fchart")
 
     @AfterTest
     fun tearDown() {
@@ -54,5 +55,37 @@ class InterpretationTest {
         val interpreter = Interpreter(res)
 
         assertThat(interpreter.run()).isEqualTo(listOf("1", "1", "0", "1"))
+    }
+
+    @Test
+    fun `first projection on turing machine trivial example`() {
+        val grammar = FlowChartGrammar()
+        val res = grammar.parse(mix.readText()).getOrThrow()
+
+        val stream = "\$fchart/turing-machine/turing-machine-int.fchart\n" +
+                "list(\"Q\",\"Qtail\",\"Instruction\",\"Operator\",\"Symbol\",\"Nextlabel\")\n" +
+                "map(list(\"Q\",list(list(\"if\",\"0\",\"goto\",\"3\"),list(\"right\"),list(\"goto\",\"0\"),list(\"write\",\"1\"))))"
+        System.setIn(stream.byteInputStream())
+        val interpreter = Interpreter(res)
+
+        val compiled = interpreter.run()
+        assertThat(compiled).isEqualTo(
+            listOf(
+                listOf("lab0:", "Left := list();",  "if eq(\"0\",firstsym(Right)) goto lab1 else lab2;"),
+                listOf("lab1:", "Right := cons(\"1\",tail(Right));", "return Right;"),
+                listOf(
+                    "lab2:",
+                    "Left := cons(firstsym(Right),Left);",
+                    "Right := tail(Right);",
+                    "if eq(\"0\",firstsym(Right)) goto lab1 else lab2;"
+                )
+            )
+        )
+        val prg = "read (Right);" + (compiled as List<List<String>>).joinToString(" ") { it.joinToString(" ") }
+        System.setIn("list(\"1\",\"1\",\"0\",\"1\",\"0\",\"1\")".byteInputStream())
+        val prgParsed = grammar.parse(prg).getOrThrow()
+        val newInterpreter = Interpreter(prgParsed)
+        val tmRes = newInterpreter.run()
+        assertThat(tmRes).isEqualTo(listOf("1", "1", "0", "1"))
     }
 }
